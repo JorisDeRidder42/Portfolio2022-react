e0cb3d8f77e32f8701e9a67590d6fdc81679ae9f
JorisDeRidder42 <65166523+JorisDeRidder42@users.noreply.github.com>
2021-12-05 10:03:53 +0100

    aanpassing css en cards

--- src/Components/home.js
+++ src/Components/home.js
@@ -5,21 +5,18 @@ import { Container, Row, Col } from 'react-bootstrap';
 const Home = () => {
     return(
         <div>
-        <Container className="containerr">
-        <Row className="blok">
-            <Col md={6}>
-                    <h1>I <span>create</span> apps, websites & brands</h1>
-                <h6 className="mt-2">I am a <b>front-end developper</b> with knowledge of <b>programming</b> and <b>graphic design.</b></h6>
+        <Container className="hoogteHome">
+            <Row className="blok">
+                <Col md={6}>
+                        <h1>I <span>create</span> apps, websites & brands</h1>
+                        <h6 className="mt-2">I am a <b>front-end developper</b> with knowledge of <b>programming</b> and <b>graphic design.</b></h6>
                         <button className="mt-4 px-5 py-3 btn btn-primary" a href="#">See my projects</button>
-            </Col>
-            <Col sm={6}>
-                {/* <Image src={require('Path1.png')} alt="hoofd"/> */}
-            </Col>
-        </Row>
+                </Col>
+                <Col sm={6}>
+                    {/* <Image src={require('Path1.png')} alt="hoofd"/> */}
+                </Col>
+            </Row>
          </Container>
-         {/* <div className="clip">
-            <div className="header"></div>
-        </div>    */}
   <HomeProjects/>
   </div>
     )
--- src/Components/homeabout.js
+++ src/Components/homeabout.js
@@ -1,40 +1,41 @@
 import React from 'react'
 import 'bootstrap';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 
 const HomeProjects = () => {
     return (
         <Container fluid>
                     <div className="bluebackground">
-                    <h2 className="tussentitel mt-5">Projects</h2>
-                        <Container>
+                    <h2 className="tussentitel mt-5 mb-5">Projects</h2>
+                    <Container>
                     <Row>
-                    <Col md={6}>
-                            <div className="allaround">
-                                <div className="card">
-                                    <div className="counter"><h3>01</h3></div>
-                                    <div className="content">
-                                        <h2>Captain Marvel</h2>
-                                        <p>Captain Marvel is a Legend in the MCU.</p>
-                                        <a href ="#" className="buttonread">Read more</a>
+                        <Col md={6} className="">
+                                <div className="allaround">
+                                    <div className="card">
+                                        <div className="counter"><h3>02</h3></div>
+                                        <div className="content">
+                                            <h2>Captain Marvel</h2>
+                                            <p>Captain Marvel is a Legend in the MCU.</p>
+                                            <a href ="#" className="buttonread">Go to this site</a>
+                                        </div>
+                                        <img src="./assets/captmarvel.png" id="captainGroot" alt="Captain Marvel"></img>
                                     </div>
-                                    <img src="./assets/captmarvel.png" id="captain" alt="Captain Marvel"></img>
                                 </div>
-                            </div>
-                        </Col> 
-                        <Col md={6}>
-                            <div className="allaround">
-                                <div className="card">
-                                    <div className="counter"><h3>02</h3></div>
-                                    <div className="content">
-                                        <h2>Sarandonga</h2>
-                                        <p>Sarandonga is a dancing company in Belgium. specialised in spanish Flamenco.</p>
-                                        <a href ="#" className="buttonread">Read more</a>
+                            </Col>
+                             
+                            <Col md={6}>
+                                <div className="allaround">
+                                    <div className="card">
+                                        <div className="counter"><h3>03</h3></div>
+                                        <div className="content">
+                                            <h2>Sarandonga</h2>
+                                            <p>Sarandonga is a dancing company in Belgium. specialised in spanish Flamenco.</p>
+                                            <a href ="#" className="buttonread">Go to this site</a>
+                                        </div>
+                                        <img src="./assets/sarandonga.png" id="malena" alt="Sarandonga Malena"></img>
                                     </div>
-                                    <img src="./assets/sarandonga.png" id="malena" alt="Sarandonga Malena"></img>
                                 </div>
-                            </div>
-                        </Col> 
+                            </Col> 
                         </Row>
                 </Container>
             </div>
