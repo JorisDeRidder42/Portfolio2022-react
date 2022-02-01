84a82d7fc37faff6dea594e50669e797737df2ee
Joris De Ridder <jorisderidder@outlook.be>
2022-02-01 12:29:31 +0100

    changed the cards

--- src/Components/homeprojects.js
+++ src/Components/homeprojects.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import 'bootstrap';
 import { Container, Row, Col } from 'react-bootstrap';
-import { CardProject } from './cardProject';
 import { Card, Button } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
 
-
-const tekst1 = {
-    title: 'Sarandonga',
-    text: 'Created a website for a flamenco dance company based in Belgium.',
-    tools: 'Bootstrap 5, HTML5, CSS3',
-    year: '2017',
-}
-const tekst2 = {
-    title: 'Gianni Claes',
-    text: 'Personal Logo for a friend.',
-    tools: 'Illustrator, photoshop',
-    year: '2020',
-}
-const tekst3 = {
-    title: 'Tournament',
-    text: 'Created a program where you can login, choose a game and register yourself for that tournament',
-    tools: 'Visual studio : C# & SQL',
-    year: '2021',
-}
 
 const HomeProjects = () => {
     return ( 
@@ -30,13 +11,28 @@ const HomeProjects = () => {
             <h1 className="mt-3 pt-5 text-center">Check out my <span>projects</span></h1>
             <Row className="my-5">
                 <Col md={4}>
-                    <CardProject {... tekst1}/>
+                    <Card>
+                        <Card.Img className="image" variant="top" alt="pc" src="./assets/gianni.png"/>
+                            <Card.Body>
+                                <Card.Title>Sarandonga</Card.Title>
+                                <Card.Text>
+                                    Created a website for a flamenco dance company based in Belgium.
+                                </Card.Text>
+                            <Card.Subtitle className="mb-2 text-muted">Tools: Bootstrap 5, HTML5, CSS3</Card.Subtitle>
+                            <Card.Subtitle className="mb-4 text-muted">Year: 2017</Card.Subtitle>
+                                <div class="d-flex justify-content-center">
+                                <LinkContainer to={'/projects'}>
+                                    <button className="mt-4 px-5 py-3 btn btn-primary" a href="#">See my projects</button>
+                                </LinkContainer>
+                            </div>
+                        </Card.Body>
+                    </Card>
                 </Col>
                 <Col md={4}>
-                    <CardProject {...tekst2}/>
+                    
                 </Col>
                 <Col md={4}>
-                    <CardProject {...tekst3}/>
+                   
                 </Col>
             </Row>
         </Container>
