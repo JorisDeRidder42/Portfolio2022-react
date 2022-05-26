d73d3b49628667f06806d7bb97b1183eb3113bb8
JorisDeRidderY <Joris.deridder@yucopia.com>
2022-05-26 11:19:33 +0200

    wip homepage

--- src/Components/homeprojects.js
+++ src/Components/homeprojects.js
@@ -15,7 +15,7 @@ const HomeProjects = () => {
           <Col md={6}>
             <Card style={{ width: "18rem" }}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Tournament app</Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
