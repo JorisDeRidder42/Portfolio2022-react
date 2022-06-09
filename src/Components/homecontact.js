fde7891dbfefcdcd9de2d13cbfe141fe4d1416f7
JorisDeRidderY <Joris.deridder@yucopia.com>
2022-06-09 22:31:07 +0200

    fix email

--- src/Components/homecontact.js
+++ src/Components/homecontact.js
@@ -61,7 +61,7 @@ export const HomeContact = () => {
                       <Form.Control
                         type="email"
                         placeholder="Email"
-                        name="email"
+                        name="Email"
                       />
                     </Form.Group>
                   </Col>
