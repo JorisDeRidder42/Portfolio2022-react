2077d3ed72c1b29b6d83bc88387bef8fe4f8ac67
JorisDeRidderY <Joris.deridder@yucopia.com>
2022-07-04 21:17:12 +0200

    fix not hardcoded projects

--- src/Components/homeprojects.js
+++ src/Components/homeprojects.js
@@ -6,8 +6,7 @@ import Tournament from "../assets/images/tournament.PNG";
 import Sarandonga from "../assets/images/sarandonga.PNG";
 import Pokemon from "../assets/video/pokedraw.gif";
 import { FiGithub, FiExternalLink } from "react-icons/fi";
-
-import { FaGithubSquare } from "react-icons/fa";
+import { project } from "../projects/projects";
 
 const HomeProjects = () => {
   return (
@@ -18,7 +17,8 @@ const HomeProjects = () => {
             <h2 className="text-start">Enkele dingen die ik heb gemaakt</h2>
           </Col>
         </Row>
-        <Container className="my-5">
+
+        {/* <Container className="my-5">
           <Row>
             <Col md={6}>
               <Card className="mt-5 card">
@@ -27,14 +27,9 @@ const HomeProjects = () => {
                     <h4 className="featured">Featured project</h4>
                   </Card.Subtitle>
                   <Card.Title>
-                    <h3>Tournament app</h3>
+                    <h3>{project.title}</h3>
                   </Card.Title>
-                  <Card.Text>
-                    Deze applicatie voor school is om ervoor te zorgen dat
-                    kinderen/jongeren zich gemakkelijk kunnen registreren voor
-                    een online spel. Alsnog kunnen ze een uur en toernooi kiezen
-                    van het gekozen spel.
-                  </Card.Text>
+                  <Card.Text>{project.description}</Card.Text>
 
                   <ul className="mt-5 project-tech-list">
                     <li>Mysql</li>
@@ -62,90 +57,7 @@ const HomeProjects = () => {
               />
             </Col>
           </Row>
-        </Container>
-
-        <Container className="my-5">
-          <Row>
-            <Col md={6}>
-              <Card className="mt-5 card">
-                <Card.Body>
-                  <Card.Title>
-                    <h3>Pokédraw app</h3>
-                  </Card.Title>
-                  <Card.Text>
-                    Een applicatie die een pokemon-database heeft a.d.h.v. een
-                    pokemon api en waar je een willekeurige Pokémon kunt trekken
-                    en natekennen.
-                  </Card.Text>
-
-                  <ul className="mt-5 project-tech-list">
-                    <li>Pokemon api</li>
-                    <li>Ionic</li>
-                  </ul>
-                  <a
-                    className="iconLink"
-                    href="https://github.com/JorisDeRidder42/pokedrawing"
-                    aria-label="Github"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <FiGithub />
-                  </a>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Image
-                src={Pokemon}
-                className="video"
-                alt="pokedraw"
-                fluid={true}
-              />
-            </Col>
-          </Row>
-        </Container>
-
-        <Container className="my-5">
-          <Row>
-            <Col md={6}>
-              <Card className="mt-5 card">
-                <Card.Body>
-                  <Card.Title>
-                    <h3>Sarandonga</h3>
-                  </Card.Title>
-                  <Card.Text>
-                    Dit is een website ontworpen voor een dans gezelschap in
-                    Lint. Sarandonga geeft flamenco lessen aan kinderen,
-                    volwassenen en bejaarden, maar geeft ook optredens en
-                    workshops.
-                  </Card.Text>
-                  <ul className="mt-5 project-tech-list">
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>Bootstrap</li>
-                  </ul>
-                  <a
-                    className="iconLink"
-                    href="http://www.sarandonga.be/"
-                    aria-label="Github"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <FiExternalLink />
-                  </a>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Image
-                src={Sarandonga}
-                className="mt-5"
-                alt="tournament"
-                fluid={true}
-              />
-            </Col>
-          </Row>
-        </Container>
+        </Container> */}
       </Container>
     </div>
   );
---
+++ src/projects/projects.js
@@ -0,0 +1,27 @@
+export const project = [
+  {
+    title: "Tournament app",
+    description:
+      "Deze applicatie voor school is om ervoor te zorgen dat kinderen/jongeren zich gemakkelijk kunnen registreren voor een online spel. Alsnog kunnen ze een uur en toernooi kiezen van het gekozen spel.",
+    image: "/src/assets/images/tournament.PNG",
+    tags: ["Mysql", "MaterialDesign", "C#"],
+    id: 0,
+    link: "https://github.com/JorisDeRidder42/DMProject_Toernooi",
+  },
+  {
+    title: "Pokedraw app",
+    description:
+      "Een applicatie die een pokemon-database heeft a.d.h.v. een pokemon api en waar je een willekeurige Pokémon kunt trekken en natekennen.",
+    image: "/src/assets/video/pokedraw.gif",
+    tags: ["Pokemon api", "Ionic"],
+    id: 1,
+  },
+  {
+    title: "Sarandonga",
+    description:
+      "Dit is een website ontworpen voor een dans gezelschap in Lint. Sarandonga geeft flamenco lessen aan kinderen, volwassenen en bejaarden, maar geeft ook optredens en workshops.",
+    image: "/src/assets/images/sarandonga.PNG",
+    tags: ["HTML5", "CSS3", "Bootstrap"],
+    id: 2,
+  },
+];
